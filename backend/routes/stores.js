cf14402b97256ad6a2c85da1a7b3a55c26112f6e
KamalLahloh <kamal.lahloh@gmail.com>
2024-01-06 23:22:25 +0300

    getAllStores (authn authz)

--- backend/routes/stores.js
+++ backend/routes/stores.js
@@ -7,11 +7,19 @@ const {
   registerStore,
   loginStore,
 } = require("../controllers/stores");
+const authentication = require("../middleware/authentication");
+const authorization = require("../middleware/authorization");
 
 // define router
 const storesRouter = express.Router();
 
-storesRouter.get("/", getAllStores);
+storesRouter.get(
+  "/",
+  authentication,
+  authorization("SEE-STORES"),
+  getAllStores
+);
+
 storesRouter.get("/:id", getStoreById);
 storesRouter.put("/:id", updateStoreById);
 storesRouter.delete("/:id", deleteStoreById);
