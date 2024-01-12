f7bf3cfb129b8aea2dd02d0e8b821a753fa9b472
KamalLahloh <kamal.lahloh@gmail.com>
2024-01-12 17:04:32 +0300

    working on Navbar bootstrap

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,49 @@
-import React, { useContext } from "react";
+//       {isLoggedIn ? (
+//         <>
+
+//           <NavLink to="/users/:id">My Account</NavLink>
+//           <NavLink to="/carts">
+//             <div className="btn btn-outline-danger" style={{ fontSize: "1.5rem", color: "black" }}>
+//               <i className="bi bi-cart-fill"></i>
+//             </div>
+//           </NavLink>
+
+//           {/*<Button
+//           type="button"
+//           variant="btn btn-outline-secondary btn-sm"
+//           onClick={logout}
+//         >
+//           Logout
+//         </Button>*/}
+//         </>
+//       ) : (
+//         <>
+//           {/* //! switch between user and store login */}
+//           <NavLink to="/stores">Sell on K-shop</NavLink>
+//           <NavLink to="/products">Home</NavLink>
+//           <NavLink to="/users/login">Login</NavLink>
+//           <NavLink to="/users/register">Register</NavLink>
+//         </>
+//       )}
+//     </div>
+//   );
+
+import { useContext } from "react";
+// eslint-disable-next-line
 import { NavLink /* useNavigate */ } from "react-router-dom";
 import { appContext } from "../App";
-// eslint-disable-next-line
-import { Button /*Alert Breadcrumb */ } from "react-bootstrap";
 
-const Navbar = () => {
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
+
+function OffcanvasExample() {
   // const navigate = useNavigate();
+
   // eslint-disable-next-line
   const { isLoggedIn, setToken, setIsLoggedIn } = useContext(appContext);
   // const logout = () => {
@@ -17,35 +55,126 @@ const Navbar = () => {
   // };
 
   return (
-    <div className="navbar navbar-expand-lg">
-      {isLoggedIn ? (
-        <>
-          <NavLink to="/products" /* reloadDocument */>Home</NavLink>
-          <NavLink to="/users/:id">My Account</NavLink>
-          <NavLink to="/carts">
-            +5
-            <i className="bi bi-cart-fill"></i>
-          </NavLink>
-
-          {/*<Button
-          type="button"
-          variant="btn btn-outline-secondary btn-sm"
-          onClick={logout}
+    <>
+      {[/* false, */ "sm" /* , 'md', 'lg', 'xl', 'xxl' */].map((expand) => (
+        <Navbar
+          key={expand}
+          expand={expand}
+          className="bg-body-tertiary mb-3 Navbar"
         >
-          Logout
-        </Button>*/}
-        </>
-      ) : (
-        <>
-          {/* //! switch between user and store login */}
-          <NavLink to="/stores">Sell on K-shop</NavLink>
-          <NavLink to="/products">Home</NavLink>
-          <NavLink to="/users/login">Login</NavLink>
-          <NavLink to="/users/register">Register</NavLink>
-        </>
-      )}
-    </div>
-  );
-};
+          <Container fluid>
+            <Navbar.Brand href="#">
+              <NavLink to="/products" /* reloadDocument */>K-shop</NavLink>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Menu
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Form className="d-flex">
+                    <Form.Control
+                      type="search"
+                      placeholder="Search"
+                      className="me-2"
+                      aria-label="Search"
+                    />
+                    <Button variant="outline-success">Search</Button>
+                  </Form>
 
-export default Navbar;
+                  {isLoggedIn ? (
+                    <>
+                      <NavDropdown
+                        title="My Account"
+                        id={`offcanvasNavbarDropdown-expand-${expand}`}
+                      >
+                        <NavDropdown.Item href="#action3">
+                          Orders
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action4">
+                          Profile
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item href="#action5">
+                          Sign Out
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                      <Nav.Link href="#action2">Fav</Nav.Link>
+                      <Nav.Link href="#action1">Cart</Nav.Link>
+                    </>
+                  ) : (
+                    <>
+                      <Nav.Link href="#action0">Login</Nav.Link>
+                    </>
+                  )}
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
+    </>
+  );
+}
+
+export default OffcanvasExample;
+
+// import React, { useContext } from "react";
+// import { NavLink /* useNavigate */ } from "react-router-dom";
+// import { appContext } from "../App";
+// // eslint-disable-next-line
+// import { Button /*Alert Breadcrumb */ } from "react-bootstrap";
+
+// const Navbar = () => {
+//   // const navigate = useNavigate();
+//   // eslint-disable-next-line
+//   const { isLoggedIn, setToken, setIsLoggedIn } = useContext(appContext);
+//   // const logout = () => {
+//   //   setToken(null);
+//   //   localStorage.removeItem("token");
+//   //   setIsLoggedIn(false);
+//   //   localStorage.removeItem("isLoggedIn");
+//   //   navigate("/");
+//   // };
+
+//   return (
+//     <div className="navbar">
+//       {isLoggedIn ? (
+//         <>
+//           <NavLink to="/products" /* reloadDocument */>Home</NavLink>
+//           <NavLink to="/users/:id">My Account</NavLink>
+//           <NavLink to="/carts">
+//             <div className="btn btn-outline-danger" style={{ fontSize: "1.5rem", color: "black" }}>
+//               <i className="bi bi-cart-fill"></i>
+//             </div>
+//           </NavLink>
+
+//           {/*<Button
+//           type="button"
+//           variant="btn btn-outline-secondary btn-sm"
+//           onClick={logout}
+//         >
+//           Logout
+//         </Button>*/}
+//         </>
+//       ) : (
+//         <>
+//           {/* //! switch between user and store login */}
+//           <NavLink to="/stores">Sell on K-shop</NavLink>
+//           <NavLink to="/products">Home</NavLink>
+//           <NavLink to="/users/login">Login</NavLink>
+//           <NavLink to="/users/register">Register</NavLink>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Navbar;
--- frontend/src/components/UserLogin.js
+++ frontend/src/components/UserLogin.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
 
 const UserLogin = () => {
   return (
-    <div className="user-login">UserLogin</div>
-  )
-}
+    <div className="user-login">
+      <Container className="col-4-md">
+        <Form>
+          <Form.Group controlId="email">
+            <Row>
+              <Col className="col-3">
+                <Form.Label>Email</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control type="email" placeholder="enter your Email" />
+                {/* <Form.Text className="text-muted">enter your Email</Form.Text> */}
+              </Col>
+            </Row>
+          </Form.Group>
+          <Form.Group className="mt-2" controlId="password">
+            <Row>
+              <Col className="col-3">
+                <Form.Label>Password</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control
+                  type="password"
+                  placeholder="enter your Password"
+                />
+              </Col>
+            </Row>
+          </Form.Group>
+          <Button className="mt-2" variant="primary" type="submit">
+            Login
+          </Button>
+        </Form>
+      </Container>
+    </div>
+  );
+};
 
-export default UserLogin
+export default UserLogin;
