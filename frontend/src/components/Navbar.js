59b9034db9424f9421cf87b1f2171cbc34936121
KamalLahloh <kamal.lahloh@gmail.com>
2024-01-13 22:17:04 +0300

    Navbar HTML React moved from bootstrap to MDB

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,26 @@
-import { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { appContext } from "../App";
 
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Offcanvas from "react-bootstrap/Offcanvas";
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBBtn,
+  MDBDropdown,
+  MDBDropdownToggle,
+  MDBDropdownMenu,
+  MDBDropdownItem,
+  MDBCollapse,
+} from "mdb-react-ui-kit";
 
-function NavbarFunction() {
-  // eslint-disable-next-line
+export default function Navbar() {
+  const [openBasic, setOpenBasic] = useState(false);
+
   const { isLoggedIn, setToken, setIsLoggedIn } = useContext(appContext);
   const logout = () => {
     setToken(null);
@@ -20,80 +30,98 @@ function NavbarFunction() {
   };
 
   return (
-    <>
-      {[/* false, */ "sm" /* , 'md', 'lg', 'xl', 'xxl' */].map((expand) => (
-        <Navbar
-          key={expand}
-          expand={expand}
-          className="bg-body-tertiary mb-3 Navbar"
+    <MDBNavbar expand="sm" light bgColor="light">
+      <MDBContainer fluid>
+        <MDBNavbarBrand href="/products">K-shop</MDBNavbarBrand>
+
+        <MDBNavbarToggler
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          onClick={() => setOpenBasic(!openBasic)}
         >
-          <Container fluid>
-            <Navbar.Brand href="/products">K-shop</Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Menu
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="/stores">Sell</Nav.Link>
-                  <Form className="d-flex">
-                    <Form.Control
-                      type="search"
-                      placeholder="Search"
-                      className="me-2"
-                      aria-label="Search"
-                    />
-                    <Button variant="outline-success">Search</Button>
-                  </Form>
-                  {isLoggedIn ? (
-                    <>
-                      <NavDropdown
-                        title="My Account"
-                        id={`offcanvasNavbarDropdown-expand-${expand}`}
-                      >
-                        <NavDropdown.Item href="#">Orders</NavDropdown.Item>
-                        <NavDropdown.Item href="/users/:id">
-                          Profile
-                        </NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="/users/login">
-                          <div onClick={logout}>Logout</div>
-                        </NavDropdown.Item>
-                      </NavDropdown>
-                      <Nav.Link className="d-flex gap-1" href="#">
-                        <i className="bi bi-heart"></i>
-                        <label className="d-md-block d-sm-none d-block">
-                          Fav
-                        </label>
-                      </Nav.Link>
-                      <Nav.Link className="d-flex gap-1" href="/carts">
-                        <i className="bi bi-cart"></i>
-                        <label className="d-md-block d-sm-none d-block">
-                          Cart
-                        </label>
-                      </Nav.Link>
-                    </>
-                  ) : (
-                    <>
-                      <Nav.Link href="/users/login">Login</Nav.Link>
-                      <Nav.Link href="/users/register">Register</Nav.Link>
-                    </>
-                  )}
-                </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
-    </>
+          <MDBIcon icon="bars" fas />
+        </MDBNavbarToggler>
+
+        <MDBCollapse navbar open={openBasic}>
+          <form className="d-flex input-group w-auto">
+            <input
+              type="search"
+              className="form-control"
+              placeholder="Type query"
+              aria-label="Search"
+            />
+            <MDBBtn color="primary">Search</MDBBtn>
+          </form>
+          <MDBNavbarNav className="mr-auto mb-2 mb-lg-0">
+            <MDBNavbarItem>
+              <MDBNavbarLink active aria-current="page" href="/stores">
+                Sell
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+
+            {isLoggedIn ? (
+              <>
+                <MDBNavbarItem>
+                  <MDBDropdown>
+                    <MDBDropdownToggle
+                      tag="a"
+                      className="nav-link"
+                      role="button"
+                    >
+                      My Account
+                    </MDBDropdownToggle>
+                    <MDBDropdownMenu>
+                      <MDBDropdownItem link>Orders</MDBDropdownItem>
+                      <MDBDropdownItem link href="/users/:id">
+                        Profile
+                      </MDBDropdownItem>
+                      <MDBDropdownItem link /* href="/users/login" */>
+                        <div onClick={logout}>Logout</div>
+                      </MDBDropdownItem>
+                    </MDBDropdownMenu>
+                  </MDBDropdown>
+                </MDBNavbarItem>
+
+                <MDBNavbarItem>
+                  <MDBNavbarLink className="d-flex gap-1" href="#">
+                    <i className="bi bi-heart"></i>
+                    <label className="d-md-block d-sm-none d-block">Fav</label>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+
+                <MDBNavbarItem>
+                  <MDBNavbarLink className="d-flex gap-1" href="/carts">
+                    <i className="bi bi-cart"></i>
+                    <label className="d-md-block d-sm-none d-block">Cart</label>
+                  </MDBNavbarLink>
+                </MDBNavbarItem>
+
+                {/* <MDBNavbarItem>
+                  <MDBNavbarLink
+                    disabled
+                    href="#"
+                    tabIndex={-1}
+                    aria-disabled="true"
+                  >
+                    Disabled
+                  </MDBNavbarLink>
+                </MDBNavbarItem> */}
+              </>
+            ) : (
+              <>
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/users/login">Login</MDBNavbarLink>
+                </MDBNavbarItem>
+
+                <MDBNavbarItem>
+                  <MDBNavbarLink href="/users/register">Register</MDBNavbarLink>
+                </MDBNavbarItem>
+              </>
+            )}
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
   );
 }
-
-export default NavbarFunction;
