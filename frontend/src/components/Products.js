958d1142810c274f5133ac092724699daa48cd52
KamalLahloh <kamal.lahloh@gmail.com>
2024-01-15 00:25:08 +0300

    show the products card on home page

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,11 +1,13 @@
 // import axios from "axios";
-import React, {  /* useEffect ,*/ useContext } from "react";
+import React, { /* useEffect ,*/ useContext } from "react";
 import { appContext } from "../App";
 
 const Products = () => {
   const {
-    token,
-    loggedInUserId,
+    // token,
+    // loggedInUserId,
+    products,
+    // setProducts,
     // isLoggedIn,
     // loggedInName,
     // setToken,
@@ -13,7 +15,6 @@ const Products = () => {
     // setLoggedInUserId,
     // setLoggedInName,
   } = useContext(appContext);
-
 
   // useEffect(() => {
   //   axios
@@ -36,44 +37,68 @@ const Products = () => {
 
   return (
     <section className="products">
-      <div className="text-center container py-5">
-        <h4 className="mt-4 mb-5">
-          <strong>Products</strong>
-        </h4>
-        <div className="d-flex align-items-center justify-content-center ">
-          <div className="col-xl-3 col-lg-4 col-md-5 col-sm-8 col-10 mb-4">
-            <div className="card">
-              <div className="bg-image hover-zoom">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/belt.webp"
-                  className="w-100"
-                />
-                <a href="#!">
-                  <div className="mask">
-                    <div className="d-flex justify-content-start align-items-end h-100">
-                      <h5>
-                        <span className="badge bg-danger ms-2">Fav</span>
-                      </h5>
+      {/* {successfulDelete && <>{successfulDelete}</>}
+      {successfulUpdate && <>{successfulUpdate}</>} */}
+
+      {products ? (
+        Array.isArray(products) && products.length > 0 ? (
+          <>
+            <div className="text-center container py-5">
+              <h4 className="mt-4 mb-5">
+                <strong>Products</strong>
+              </h4>
+              <div className="d-flex flex-wrap align-items-center justify-content-center">
+                {products.map((product) => {
+                  return (
+                    <div className="col-xl-3 col-lg-4 col-md-5 col-sm-8 col-10 m-4">
+                      <div
+                        className="card product"
+                        key={product._id.$oid}
+                        id={product._id.$oid}
+                      >
+                        <div className="bg-image hover-zoom">
+                          <img
+                            src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/E-commerce/Products/belt.webp"
+                            className="w-100"
+                            alt=""
+                          />
+                          <a href="#!">
+                            <div className="mask">
+                              <div className="d-flex justify-content-start align-items-end h-100">
+                                <h5>
+                                  <span className="badge bg-danger ms-2">
+                                    Fav
+                                  </span>
+                                </h5>
+                              </div>
+                            </div>
+                            <div className="hover-overlay">
+                              <div className="mask"></div>
+                            </div>
+                          </a>
+                        </div>
+                        <div className="card-body">
+                          <a href="/">
+                            <h5 className="card-title mb-3">Product name</h5>
+                          </a>
+                          <a href="/" className="text-reset">
+                            <p>Category</p>
+                          </a>
+                          <h6 className="mb-3">$61.99</h6>
+                        </div>
+                      </div>
                     </div>
-                  </div>
-                  <div className="hover-overlay">
-                    <div className="mask"></div>
-                  </div>
-                </a>
-              </div>
-              <div className="card-body">
-                <a href="">
-                  <h5 className="card-title mb-3">Product name</h5>
-                </a>
-                <a href="" className="text-reset">
-                  <p>Category</p>
-                </a>
-                <h6 className="mb-3">$61.99</h6>
+                  );
+                })}
               </div>
             </div>
-          </div>
-        </div>
-      </div>
+          </>
+        ) : (
+          <div>{products}</div>
+        )
+      ) : (
+        <div>No Products Yet</div>
+      )}
     </section>
   );
 };
